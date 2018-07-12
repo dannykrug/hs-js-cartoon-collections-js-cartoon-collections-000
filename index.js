69916d53175badce316c5f61989e34e7d4ceb5ea
Daniel Krug <dannykrug@gmail.com>
2018-07-12 00:47:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  if(dwarves.length >= 1) {
+    let new_array = []
+    for(let i = 0; i < dwarves.length; i++) {
+      new_array.push(`${i+1}. ${dwarves[i]} `)
+    }
+    return(new_array.join(""))
+  }
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  if(planeteerCalls.length >= 1) {
+    let heIsOurHero = []
+    for(let i = 0; i < planeteerCalls.length; i++) {
+      heIsOurHero.push(planeteerCalls[i].toUpperCase + '!')
+    }
+    return heIsOurHero
+  }
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+    if(words[i].length > 4)
+    return true
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  let cheese = ['gouda', 'cheddar', 'camembert']
+  for(let i = 0; i < foods.length; i++) {
+    for(let c = 0; c < cheese.length; c++) {
+      if(foods[i] === cheese[c]){
+        return foods[i]
+      }
+    }
+  }
+
+  return 'no cheese!'
 }
